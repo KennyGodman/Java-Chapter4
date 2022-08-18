298623b720ded521e69f7d26e7f2e5b492e165fb
KennyGodman <107128812+KennyGodman@users.noreply.github.com>
2022-08-18 04:45:30 +0100

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,29 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args){
+        print();
+    }
+
+        private static void print(){
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter weight in pounds");
+            double weightInPounds = input.nextDouble();
+
+            System.out.print("Enter height in metres");
+            double heightInMetres = input.nextDouble();
+
+            double weightInKg = weightInPounds * 0.45359237;
+            double heightInInches = heightInMetres * 0.0254;
+
+            double heightInInchesSquare = heightInInches * heightInInches;
+
+            double bmi = weightInKg / heightInInchesSquare;
+            System.out.printf("BMI is %f", bmi);
+
+        }
+}
